55c499db678513d3a8298a9b5d976fa35f52b207
vwxyzjimmy <vwxyzjimmy@gmail.com>
2019-03-25 18:04:51 +0800

    complete code requirement

--- blink.c
+++ blink.c
@@ -2,9 +2,9 @@
 #include "reg.h"
 
 /**
- * 
+ *
  * LED init
- * 
+ *
  */
 void led_init(unsigned int led)
 {
@@ -24,19 +24,50 @@ void led_init(unsigned int led)
 	//PUPDR led pin = 00 => No pull-up, pull-down
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(led));
 	CLEAR_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(led));
+
+}
+void gpio_input_init(unsigned int port, unsigned int input){
+
+	SET_BIT(RCC_BASE + RCC_AHB1ENR_OFFSET, GPIO_EN_BIT(GPIO_PORTA));
+
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_1_BIT(input));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_MODER_OFFSET, MODERy_0_BIT(input));
+
+	SET_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_1_BIT(input));
+	CLEAR_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_PUPDR_OFFSET, PUPDRy_0_BIT(input));
+
 }
 
+
 /**
- * 
+ *
  * blink LED forever
- * 
+ *
  */
 void blink(unsigned int led)
 {
 	led_init(led);
+	unsigned int button = 14;
+	led_init(button);
+	gpio_input_init(GPIO_PORTA, 0);
 
-	unsigned int i;
+	unsigned int i ;
+	//
 
+	while(!READ_BIT(GPIO_BASE(GPIO_PORTA) + GPIOx_IDR_OFFSET, ODy_BIT(0))){
+		//set GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BSy_BIT(14));
+
+		for (i = 0; i < 100000; i++)
+			;
+
+		//reset GPIOD led pin
+		SET_BIT(GPIO_BASE(GPIO_PORTD) + GPIOx_BSRR_OFFSET, BRy_BIT(14));
+
+		for (i = 0; i < 100000; i++)
+			;
+
+	};
 	while (1)
 	{
 		//set GPIOD led pin
@@ -54,9 +85,9 @@ void blink(unsigned int led)
 }
 
 /**
- * 
+ *
  * blink LED x count
- * 
+ *
  */
 void blink_count(unsigned int led, unsigned int count)
 {
